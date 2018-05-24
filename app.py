c4a161f2af9db3e208f65b45ef9a536a6003cdb0
Théo <39556040+Assoura@users.noreply.github.com>
2018-05-24 14:07:29 +0200

    e

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    print("##################   "+message['message']['text']
+                    print("##################   "+message['message']['text'])
                     bot.send_text_message(recipient_id, "Got you !")
     return "Message Processed"
 
