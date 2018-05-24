a6263d25b7e29f5c6a0a893a797d66bc0f3db4b3
Théo <39556040+Assoura@users.noreply.github.com>
2018-05-24 17:49:43 +0200

    Bot 1

--- app.py
+++ app.py
@@ -53,12 +53,11 @@ def receive_message():
                             }
                             spot = commande.split(' ')[1]
                             site = commande.split(' ')[2]
-                            bot.send_text_message(recipient_id,'Je ne peux faire que ça pour le moment :')
-                            bot.send_text_message(recipient_id,url[spot][site])
+                            bot.send_text_message(recipient_id,'Je ne peux faire que ça pour le moment :'+url[spot][site])
                         except:
-                            bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les site 'msw' et 'surf_report' et les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Et ne comprends que la syntaxe 'Mirmoc spot site' ''')
+                            bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les site 'msw' et 'surf_report' et les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Je ne comprends que la syntaxe 'Mirmoc spot site' ''')
                     else:
-                        bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les site 'msw' et 'surf_report' et les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Et ne comprends que la syntaxe 'Mirmoc spot site' ''')
+                        bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les site 'msw' et 'surf_report' et les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Je ne comprends que la syntaxe 'Mirmoc spot site' ''')
                             #print("##################  Message reçu : "+message['message']['text'])
                             #get_message('Siouville')
                             #print("##################  Sortie fonction report")
