0d44a032513debb9d8a16ee2d668dc3642b51053
Théo <39556040+Assoura@users.noreply.github.com>
2018-05-27 23:05:52 +0200

    s

--- app.py
+++ app.py
@@ -33,17 +33,18 @@ def receive_message():
             commande = message['message']['text']
         except:
             print('Message non pris en compte')
-        print(recipient_id+' a envoyé : '+commande+''' à l'instant : '''+str(message['timestamp']))
-        if message['message'].get('text') and "Mirmoc" in message['message']['text'] and commande != commande0 :
-            try:
-                scraping(commande,recipient_id)
-                send_report(recipient_id)
-            except:
-                print('Erreur')
-                bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Je ne comprends que la syntaxe 'Mirmoc spot' ''')
         else:
-            bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Je ne comprends que la syntaxe 'Mirmoc spot' ''')
-        commande0 = commande
+            print(recipient_id+' a envoyé : '+commande+''' à l'instant : '''+str(message['timestamp']))
+            if message['message'].get('text') and "Mirmoc" in message['message']['text'] and commande != commande0 :
+                try:
+                    scraping(commande,recipient_id)
+                    send_report(recipient_id)
+                except:
+                    print('Erreur')
+                    bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Je ne comprends que la syntaxe 'Mirmoc spot' ''')
+            else:
+                bot.send_text_message(recipient_id,'''Désolé, je n'ai pas compris. Je ne connais que les spots 'Seignosse', 'Siouville', 'La_torche', 'Vendee', 'Quiberon' et 'Etretat'. Je ne comprends que la syntaxe 'Mirmoc spot' ''')
+            commande0 = commande
     return "Message Processed"
 
 
