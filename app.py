f9716fc4f37aeca7e2d1b680b621acab5bdaefc2
Théo <39556040+Assoura@users.noreply.github.com>
2018-05-27 19:47:06 +0200

    s

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Python libraries that we need to import for our bot
 import json
-import request
+import requests
 from PIL import Image
 from selenium import webdriver
 from flask import Flask, requests
